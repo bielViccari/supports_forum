07c0afa08ef387a95728e8cb60ddb6f5c033a6f0
Gabriel Viccari <gabriel.viccari20@gmail.com>
2024-04-30 11:02:40 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\SupportController;
+use App\Http\Controllers\Site\SiteController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/supports', [SupportController::class, 'index'])->name('support.index');
+
+Route::get('/contato', [SiteController::class, 'contact']);
+Route::get('/', function () {
+    return view('welcome');
+});
